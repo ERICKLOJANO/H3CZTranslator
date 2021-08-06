57ab39826871a957f35f7e9be6b1df06f2cb0d94
ErickLojano <erickjlojano@gmail.com>
2021-08-06 16:11:44 -0400

    added a gitignore file for keys and other stuff

--- config.js
+++ config.js
@@ -4,5 +4,3 @@ module.exports ={
     access_token:"1420780247327383557-jsDt0aWdnMpZTipnJ3FBZAAn3NmxcF",
     access_token_secret:"L6azMrSBBHlH8QJVVefJa1aMMgJzMVTBOnA9zx2VK2qWH"
 }
-//key = KEY_API
-//AIzaSyDljboOBNOS4p-S70lTJ23YlrVY9CJpUQM
